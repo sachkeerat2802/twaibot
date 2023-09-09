6d05baf5fb2ce3bfa3f5b3f86b0a5a7cd78ac57c
Sachkeerat Singh <sachkeerat2802@gmail.com>
2023-09-10 03:31:28 +0530

    initialized twitter api and callback url

--- functions/index.js
+++ functions/index.js
@@ -3,3 +3,17 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const databaseReference = admin.firestore().doc("tokens/twaibot");
+
+const twAPI = require("twitter-api-v2").default;
+const twClient = new twAPI({
+    clientId: "",
+    clientSecret: "",
+});
+
+const callbackURL = "http://127.0.0.1:5000/twaibot/us-central1/callback";
+
+exports.auth = functions.https.onRequest((request, response) => {});
+
+exports.callback = functions.https.onRequest((request, response) => {});
+
+exports.tweet = functions.https.onRequest((request, response) => {});
