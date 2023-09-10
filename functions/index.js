02e998002e5809a78bb610baa46376b4d6318462
Sachkeerat Singh <sachkeerat2802@gmail.com>
2023-09-11 02:41:11 +0530

    small changes

--- functions/index.js
+++ functions/index.js
@@ -11,16 +11,38 @@ const twClient = new twAPI({
     clientSecret: process.env.CLIENT_SECRET,
 });
 
-const authURL = "http://127.0.0.1:5000/twaibot/us-central1/auth";
 const callbackURL = "http://127.0.0.1:5000/twaibot/us-central1/callback";
 
 exports.auth = functions.https.onRequest(async (request, response) => {
     const { url, codeVerifier, state } = twClient.generateOAuth2AuthLink(callbackURL, { scope: ["tweet.read", "tweet.write", "users.read", "offline.access"] });
-
     await databaseReference.set({ codeVerifier, state });
-
     response.redirect(url);
 });
 
-exports.callback = functions.https.onRequest((request, response) => {});
+exports.callback = functions.https.onRequest(async (request, response) => {
+    const { state, code } = request.query;
+    const datatbaseSnapshot = await databaseReference.get();
+    const { codeVerifier, state: storedState } = datatbaseSnapshot.data();
+
+    if (state !== storedState) {
+        return response.status(400).send("Tokens do not match!");
+    }
+
+    const {
+        client: loggedClient,
+        accessToken,
+        refreshToken,
+    } = await twClient.loginWithOAuth2({
+        code,
+        codeVerifier,
+        redirectUri: callbackURL,
+    });
+
+    await databaseReference.set({ accessToken, refreshToken });
+
+    const { data } = await loggedClient.v2.me();
+
+    response.sendStatus(200);
+});
+
 exports.tweet = functions.https.onRequest((request, response) => {});
