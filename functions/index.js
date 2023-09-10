21c64f40b12b466b873bff75f1cb317a06c5be8a
Sachkeerat Singh <sachkeerat2802@gmail.com>
2023-09-10 05:50:03 +0530

    added env variables and wrote auth logic

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,26 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
+const twAPI = require("twitter-api-v2").default;
+require("dotenv").config();
 
+admin.initializeApp();
 const databaseReference = admin.firestore().doc("tokens/twaibot");
 
-const twAPI = require("twitter-api-v2").default;
 const twClient = new twAPI({
-    clientId: "",
-    clientSecret: "",
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
 });
 
+const authURL = "http://127.0.0.1:5000/twaibot/us-central1/auth";
 const callbackURL = "http://127.0.0.1:5000/twaibot/us-central1/callback";
 
-exports.auth = functions.https.onRequest((request, response) => {});
+exports.auth = functions.https.onRequest(async (request, response) => {
+    const { url, codeVerifier, state } = twClient.generateOAuth2AuthLink(callbackURL, { scope: ["tweet.read", "tweet.write", "users.read", "offline.access"] });
+
+    await databaseReference.set({ codeVerifier, state });
+
+    response.redirect(url);
+});
 
 exports.callback = functions.https.onRequest((request, response) => {});
-
 exports.tweet = functions.https.onRequest((request, response) => {});
